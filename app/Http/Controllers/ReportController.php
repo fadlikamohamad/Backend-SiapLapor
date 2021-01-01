81fe959a876bf5c86c3e7fb9faff06b6654ff3da
Hakim Akbaru Sulthony <hakimakbar21@gmail.com>
2021-01-01 09:08:15 +0700

    fixing ReportController

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -4,15 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Report;
+use Carbon\Carbon;
 
 class ReportController extends Controller
 {
     public function get_all_report() {
-        return response()->json(Report::all(), 200);
+        //return response()->json(Report::all(), 200);
+
+        $list_report = Report::all();
+        return response([
+            'status' => true,
+            'message' => 'Daftar Report',
+            'data' => $list_report
+        ], 200);
     }
 
     public function get_detail_report($id) {
-        return response()->json(Report::where('id', $id)->get(), 200);
+        $report = Report::where('id', $id)->get();
+        return response([
+            'status' => true,
+            'message' => 'Detail Report',
+            'data' => $report
+        ], 200);
+        //return response()->json(Report::where('id', $id)->get(), 200);
     }
 
     public function delete_data_report($id) {
@@ -32,7 +46,13 @@ class ReportController extends Controller
     }
 
     public function get_user_report($userId) {
-        return response()->json(Report::where('user_id', $userId)->get(), 200);
+        // return response()->json(Report::where('user_id', $userId)->get(), 200);
+        $reports = Report::where('user_id', $userId)->get();
+        return response([
+            'status' => true,
+            'message' => 'User Report',
+            'data' => $reports
+        ], 200);
     }
 
     public function insert_data_report(Request $request) {
@@ -43,6 +63,7 @@ class ReportController extends Controller
         $insert_report->alamat = $request->alamat;
         $insert_report->isi_laporan = $request->isi_laporan;
         $insert_report->user_id = $request->user_id;
+        $insert_report->created_at = Carbon::now();
         $insert_report->save();
         return response([
             'status' => true,
